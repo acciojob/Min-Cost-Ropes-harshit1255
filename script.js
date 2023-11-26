f2868470023500ee1a690d7aa7779586abe385f0
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-27 03:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ function mincost(arr)
 	let res =0;
 	while(arr.length!=1)
 		{ 
-			arr.sort();
+			arr.sort((a,b)=>
+				{
+					if(a>b)
+						return b-a;
+					else return a - b;
+				});
 			let sum = arr[0]+arr[1];
 			res +=sum;
 			arr.splice(0,2,sum);
